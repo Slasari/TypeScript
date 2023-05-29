b6298fe8da9040e667a9023abcf220107f1acc11
Slasari <ariel_salcito@live.com>
2023-05-29 20:00:21 -0300

    Update calculadora.ts

--- calculadora.ts
+++ calculadora.ts
@@ -5,7 +5,29 @@
 function sumar(x:number, y:number): number {
     let resultado = x + y;
     console.log(`${x} + ${y} = ${resultado}`)
-    return resultado;
+    return resultado; 
 };
 
+// Function anonima, utiliza "callback"
+
+let restar = function(x:number, y:number) : number {
+    let resultado = x - y;
+    console.log(`${x} - ${y} = ${resultado}`)
+    return resultado;
+}
+
+let multiplicar: (x: number, y: number) => number
+
+// Forma larga
+multiplicar = function(x, y){
+    let resultado = x * y
+    console.log(`${x} * ${y} = ${resultado}`)
+    return resultado
+}
+// Forma corta
+multiplicar = (x, y) => x * y
+
+
+
 sumar(2,8);
+restar(9,5);
